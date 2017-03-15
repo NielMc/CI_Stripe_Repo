f9f62a01f32c9a905e0f8400b95f77bdea4c1fd4
Richard Dalton <richard@devjoy.com>
2017-03-15 22:36:39 +0000

    Added Login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import profile, logout
+from .views import profile, logout, login
 
 urlpatterns = [
     url(r'^profile$', profile, name='profile'),
-    url(r'^logout$', logout, name='logout')
+    url(r'^logout$', logout, name='logout'),
+    url(r'^login$', login, name='login'),
 ]
