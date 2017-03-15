b8e15384889baacf4f6b7531f6ee8bd84260261b
Richard Dalton <richard@devjoy.com>
2017-03-15 22:18:11 +0000

    Add logout

--- OurSimpleStore/urls.py
+++ OurSimpleStore/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from home.views import get_index
+from accounts import urls as accounts_urls
 from products import urls as products_urls
 from django.views import static
 from settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include(accounts_urls)),
     url(r'^$', get_index, name='index'),
     url(r'^products/', include(products_urls)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib import messages, auth
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
 
 # Create your views here.
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'You have successfully logged out')
+    return redirect(reverse('index'))
