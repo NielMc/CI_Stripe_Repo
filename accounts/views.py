4e953d4b1bd589db181a0e4d73849c9e4f82f987
Richard Dalton <richard@devjoy.com>
2017-03-15 22:31:13 +0000

    Added Profile Page

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,17 @@
 from django.contrib import messages, auth
 from django.core.urlresolvers import reverse
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+
+@login_required(login_url='/login?next=profile')
+def profile(request):
+    return render(request, 'profile.html')
+
 
 # Create your views here.
 def logout(request):
     auth.logout(request)
     messages.success(request, 'You have successfully logged out')
     return redirect(reverse('index'))
+
