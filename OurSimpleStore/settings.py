63928a3e2f96b76c0371277f1dc0736488ab52cb
Richard Dalton <richard@devjoy.com>
2017-03-15 09:29:07 +0000

    Fixed Allowed Hosts

--- OurSimpleStore/settings.py
+++ OurSimpleStore/settings.py
@@ -29,8 +29,6 @@ ALLOWED_HOSTS = ['com-devjoy-oursimplestore.herokuapp.com', '127.0.0.1']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
