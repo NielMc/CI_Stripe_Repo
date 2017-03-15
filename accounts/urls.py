cfb812cdfe489d5be97aae9a72b4160d50611a2f
Richard Dalton <richard@devjoy.com>
2017-03-15 23:09:14 +0000

    Added Register Form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import profile, logout, login
+from .views import profile, logout, login, register
 
 urlpatterns = [
     url(r'^profile$', profile, name='profile'),
     url(r'^logout$', logout, name='logout'),
     url(r'^login$', login, name='login'),
+    url(r'^register$', register, name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.template.context_processors import csrf
-from .forms import UserLoginForm
+from .forms import UserLoginForm, UserRegistrationForm
 
 
 @login_required(login_url='/login?next=profile')
@@ -44,3 +44,30 @@ def login(request):
     args = {'form': form, 'next': request.GET['next'] if request.GET and 'next' in request.GET else ''}
     args.update(csrf(request))
     return render(request, 'login.html', args)
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            user = auth.authenticate(username=request.POST.get('username'),
+                                     password=request.POST.get('password1'))
+
+            if user:
+                auth.login(request, user)
+                messages.success(request, "You have successfully registered")
+                return redirect(reverse('profile'))
+
+            else:
+                messages.error(request, "unable to log you in at this time!")
+
+    else:
+        form = UserRegistrationForm()
+
+    args = {'form': form}
+    args.update(csrf(request))
+
+    return render(request, 'register.html', args)
